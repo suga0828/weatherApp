fcb4963c5fcc2f6a8ca760f0179cc855f335efb5
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-07-28 12:51:34 -0500

    Implements get on weatherService, in dev move

--- src/app/services/current-weather.service.ts
+++ src/app/services/current-weather.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
 import { Subject, Observable } from 'rxjs';
+
+import { Coords } from '../interfaces/coords';
+
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +17,20 @@ export class CurrentWeatherService {
   public $weather: Observable<any> = this.weatherSubject.asObservable();
 
   constructor(private http: HttpClient) {
-    this.get();
+    this.get({
+      lat: 4.606880,
+      lon: -74.071838
+    });
   }
 
-  get() {
-    let observable = this.http.get('assets/weather.json').subscribe(this.weatherSubject);
+  get(coords: Coords) {
+    let args: string = `?lat=${coords.lat}&lon=${coords.lon}}&appid=${environment.weatherKey}&units=metric`;
+    let url = environment.weatherEndpoint + args;
+
+    if (isDevMode()) {
+      url = 'assets/weather.json';
+    }
+
+    this.http.get(url).subscribe(this.weatherSubject);
   }
 }
